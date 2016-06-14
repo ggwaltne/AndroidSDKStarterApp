6e69d19ef86b9dbd3b527e17f0cc861bb20f0046
Gage Gwaltney <ggwaltne@harding.edu>
2016-06-14 13:57:52 -0700

    Added project files

---
+++ app/src/main/java/com/example/edjd/testapplicaton/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.edjd.testapplicaton;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void OnClick (View view) {
+        text=new TextView(this);
+        text=(TextView)findViewById(R.id.text);
+        text.setText("It's Alive!!!");
+    }
+
+}
