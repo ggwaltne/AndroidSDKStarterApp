4b9b7e0dfd367ba10d1ba829f65e4619283cd0a7
Gage Gwaltney <ggwaltne@harding.edu>
2016-06-15 15:35:24 -0700

    Added Game logic

--- app/src/main/java/com/example/edjd/testapplicaton/GameActivity.java
+++ app/src/main/java/com/example/edjd/testapplicaton/GameActivity.java
@@ -4,20 +4,37 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
-    Intent LeaderBoard;
+    Intent GameOver;
     int differencesFound = 0;
     int maxDifferences = 3;
+    TextView score;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        LeaderBoard = new Intent(getApplicationContext(), LeaderBoard.class);
+        GameOver = new Intent(getApplicationContext(), GameOverActivity.class);
+        differencesFound = 0;
+        score = (TextView)findViewById(R.id.score);
     }
 
-    public void goToGameOver (View view) {
-        startActivity(LeaderBoard);
+
+    public void onClickAddFound (View view) {
+        differencesFound++;
+
+        score.setText(Integer.toString(differencesFound));
+        if(differencesFound >= maxDifferences)
+        {
+            goToGameOver();
+            differencesFound = 0;
+            score.setText(Integer.toString(differencesFound));
+        }
+    }
+
+    public void goToGameOver () {
+        startActivity(GameOver);
     }
 }
