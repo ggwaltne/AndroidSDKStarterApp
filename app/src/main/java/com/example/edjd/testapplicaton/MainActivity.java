38d16d16a6b197296e345e02cfc1a4efce373f4a
Gage Gwaltney <ggwaltne@harding.edu>
2016-06-15 10:43:03 -0700

    Linking between activities worksgit status

--- app/src/main/java/com/example/edjd/testapplicaton/MainActivity.java
+++ app/src/main/java/com/example/edjd/testapplicaton/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.edjd.testapplicaton;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,10 +9,18 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Intent i;
     TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        i = new Intent(getApplicationContext(), LeaderBoard.class);
+
     }
+    public void OnClickLeaderBoard (View view) {
+        startActivity(i);
+    }
+
+
 }
