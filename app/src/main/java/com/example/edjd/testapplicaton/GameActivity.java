d5ba83c6f5ba8f7e5897b57e5d5e20d455ac7636
Gage Gwaltney <ggwaltne@harding.edu>
2016-06-15 12:03:55 -0700

    Added an Activity the correct Way

--- app/src/main/java/com/example/edjd/testapplicaton/GameActivity.java
+++
@@ -1,9 +0,0 @@
-package com.example.edjd.testapplicaton;
-
-import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by edjd on 6/15/2016.
- */
-public class GameActivity extends AppCompatActivity {
-}
