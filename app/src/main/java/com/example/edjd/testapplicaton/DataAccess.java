028908cd085b03f09fddcfbeae0fb808473659d5
robertmatthewcook <robertmatthewcook@gmail.com>
2016-06-15 17:22:17 -0500

    nosql

--- app/src/main/java/com/example/edjd/testapplicaton/DataAccess.java
+++ app/src/main/java/com/example/edjd/testapplicaton/DataAccess.java
@@ -1,5 +1,6 @@
 package com.example.edjd.testapplicaton;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.provider.BaseColumns;
@@ -12,40 +13,45 @@ import java.util.Map;
  * Created by robertmatthewcook on 6/15/2016.
  */
 
-/* Inner class that defines the table contents
-public static abstract class MarketRow implements BaseColumns {
-    public static final String MARKET = "market";
-    public static final String MARKET_ID = "market_id";
-    public static final String MARKET_NAME = "market_name";
-}
-
-        String[] marketRecord = {
-                MarketRow.MARKET_ID,
-                MarketRow.MARKET_NAME
-        };
-        Cursor cMarket = db.query(
-                MarketRow.MARKET,       // The table to query
-                marketRecord,           // The columns to return
-                null,                   // The columns for the WHERE clause
-                null,                   // The values for the mDbHelperWHERE clause
-                null,                   // don't group the rows
-                null,                   // don't filter by row groups
-                null                    // The sort order
-        );
-
-        cMarket.moveToFirst();
-        long marketID = cMarket.getLong(cMarket.getColumnIndexOrThrow(MarketRow.MARKET_ID));
-
-*/
-
 public class DataAccess {
 
+    private long nextScoreID(SQLiteDatabase db) {
+        long scoreID;
+        String query = "SELECT coalesce(select MAX(CAST(score_id as integer)) + 1 from score), 1)";
+        Cursor results = db.rawQuery(query, null);
+        results.moveToFirst();
+        scoreID = results.getLong(0);
+        return scoreID;
+    }
+    public void addScore(SQLiteDatabase db, long score, long user_id) {
+/*
+// Gets the data repository in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+// Create a new map of values, where column names are the keys
+        ContentValues values = new ContentValues();
+        values.put(FeedEntry.COLUMN_NAME_ENTRY_ID, id);
+        values.put(FeedEntry.COLUMN_NAME_TITLE, title);
+        values.put(FeedEntry.COLUMN_NAME_CONTENT, content);
+
+// Insert the new row, returning the primary key value of the new row
+        long newRowId;
+        newRowId = db.insert(
+                FeedEntry.TABLE_NAME,
+                FeedEntry.COLUMN_NAME_NULLABLE,
+                values);
+
+        ContentValues score = new ContentValues();
+        score.put();
+        score.put();
+        score.put();
+        score.put();
+*/
+    }
     public Map readScoreboard(SQLiteDatabase db) {
         Map scoreboard = new HashMap();
-        String selectQuery = "SELECT user.username, score.score_value FROM score JOIN user ON user.user_id = score.user_id WHERE score.game_id = '101' AND score.score_date = '2016-06-15' ORDER BY score.score_value DESC, user.username";
-
-        Cursor results = db.rawQuery(selectQuery, null);
-
+        String query = "SELECT user.user_name, score.score_value FROM score JOIN user ON user.user_id = score.user_id WHERE score.game_id = '101' AND score.score_date = '2016-06-14' ORDER BY score.score_value DESC, user.user_name";
+        Cursor results = db.rawQuery(query, null);
         if (results.moveToFirst()) {
             do {
                 scoreboard.put(results.getString(0), results.getLong(1));;
