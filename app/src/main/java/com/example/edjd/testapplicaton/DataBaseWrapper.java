0d7af3375ecc7aedabe9e06a72f9432c91fa8e69
robertmatthewcook <robertmatthewcook@gmail.com>
2016-06-15 15:56:53 -0500

    nosql

---
+++ app/src/main/java/com/example/edjd/testapplicaton/DataBaseWrapper.java
@@ -0,0 +1,64 @@
+/**
+ * Created by robertmatthewcook on 6/15/2016.
+ */
+
+package com.example.edjd.testapplicaton;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+//DataBaseWrapper mDbHelper = new DataBaseWrapper(getContext());
+//SQLiteDatabase db = mDbHelper.getWritableDatabase();
+//SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+public class DataBaseWrapper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "GageJuiceBar.db";
+    public static final int DATABASE_VERSION = 1;
+
+    private static final String SQL_CREATE_USER = "CREATE TABLE IF NOT EXISTS user(user_id TEXT, user_name TEXT, user_email TEXT, user_create_date TEXT, user_verified_date TEXT, user_deactivated_date TEXT);";
+    private static final String SQL_CREATE_GAME = "CREATE TABLE IF NOT EXISTS game(game_id TEXT, game_name TEXT, game_start_date TEXT, game_end_date TEXT);";
+    private static final String SQL_CREATE_SCORE = "CREATE TABLE IF NOT EXISTS score(score_id TEXT, game_id TEXT, user_id TEXT, score_value INTEGER, score_date TEXT);";
+    private static final String SQL_CREATE_MARKET = "CREATE TABLE IF NOT EXISTS market(market_id TEXT, market_name TEXT);";
+    private static final String SQL_CREATE_ADVERTISEMENT = "CREATE TABLE IF NOT EXISTS advertisement(advertisement_id TEXT, advertisement_url);";
+    private static final String SQL_CREATE_ADVERTISEMENT_MARKET = "CREATE TABLE IF NOT EXISTS advertisement_market(advertisement_id TEXT, market_id TEXT);";
+    private static final String SQL_CREATE_LISTING = "CREATE TABLE IF NOT EXISTS listing(listing_id TEXT, advertisement_id TEXT, listing_date TEXT);";
+    private static final String SQL_CREATE_CLICK = "CREATE TABLE IF NOT EXISTS click(click_id TEXT, listing_id TEXT, click_date TEXT);";
+
+    private static final String SQL_POPULATE_MARKET = "INSERT INTO market SELECT data.* FROM (select 'A' AS market_id, 'Auto' AS market_name UNION ALL SELECT 'H', 'Home' UNION ALL SELECT 'L', 'Life') as data LEFT JOIN market ON market.market_id = data.market_id WHERE market.market_id IS NULL;";
+    private static final String SQL_POPULATE_ADVERTISEMENT = "INSERT INTO advertisement SELECT data.* FROM (select '9' AS advertisement_id, 'http://s.f/Auto' AS advertisement_url UNION ALL SELECT '8', 'http://s.f/Home' UNION ALL SELECT '7', 'http://s.f/Life') as data LEFT JOIN advertisement ON advertisement.advertisement_id = data.advertisement_id WHERE advertisement.advertisement_id IS NULL;";
+    private static final String SQL_POPULATE_ADVERTISEMENT_MARKET = "INSERT INTO advertisement_market SELECT data.* FROM (select '9' AS advertisement_id, 'A' as market_id UNION ALL SELECT '8', 'H' UNION ALL SELECT '7', 'L') as data LEFT JOIN advertisement_market ON advertisement_market.advertisement_id = data.advertisement_id AND advertisement_market.market_id = data.market_id WHERE advertisement_market.market_id IS NULL;";
+
+    private static final String SQL_GENERATE_GAME = "INSERT INTO game SELECT data.* FROM (select '101' AS game_id, 'ExGame', '2016-06-14', '2016-06-14' UNION ALL SELECT '102', 'WhyGame', '2016-06-15', NULL) as data LEFT JOIN game ON game.game_id = data.game_id WHERE game.game_id IS NULL;";
+    private static final String SQL_GENERATE_USER = "INSERT INTO user SELECT data.* FROM (select '1' AS user_id, 'UserOne' AS user_name, 'one@fake.it' AS user_email, '2016-01-01' AS user_create_date, '2016-01-01' AS user_verified_date, NULL AS user_deactivated_date UNION ALL SELECT '2', 'UserTwo', 'two@fake.it', '2016-01-02', '2016-01-02', NULL UNION ALL SELECT '3', 'UserThree', 'three@fake.it', '2016-01-03', '2016-01-03', NULL) as data LEFT JOIN user ON user.user_id = data.user_id WHERE user.user_id IS NULL;";
+    private static final String SQL_GENERATE_SCORE = "INSERT INTO score SELECT data.* FROM (select '1001' AS score_id, 101 AS game_id, 3 AS user_id, 54321 AS score_value, '2016-06-14' AS score_date UNION ALL SELECT 1002, 101, 2, 2468, '2016-06-14' UNION ALL SELECT 1003, 101, 1, 11111, '2016-06-14') as data LEFT JOIN score ON score.score_id = data.score_id WHERE score.score_id IS NULL;";
+
+    public DataBaseWrapper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_USER);
+        db.execSQL(SQL_CREATE_GAME);
+        db.execSQL(SQL_CREATE_SCORE);
+        db.execSQL(SQL_CREATE_MARKET);
+        db.execSQL(SQL_CREATE_ADVERTISEMENT);
+        db.execSQL(SQL_CREATE_ADVERTISEMENT_MARKET);
+        db.execSQL(SQL_CREATE_CLICK);
+        db.execSQL(SQL_CREATE_LISTING);
+        this.doPopulateData(db);
+        this.doGenerateData(db);
+    }
+    public void doPopulateData(SQLiteDatabase db) {
+        db.execSQL(SQL_POPULATE_MARKET);
+        db.execSQL(SQL_POPULATE_ADVERTISEMENT);
+        db.execSQL(SQL_POPULATE_ADVERTISEMENT_MARKET);
+    }
+    public void doGenerateData(SQLiteDatabase db) {
+        db.execSQL(SQL_GENERATE_GAME);
+        db.execSQL(SQL_GENERATE_USER);
+        db.execSQL(SQL_GENERATE_SCORE);
+    }
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        onCreate(db);
+    }
+}
--- app/src/main/java/com/example/edjd/testapplicaton/MainActivity.java
+++ app/src/main/java/com/example/edjd/testapplicaton/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.edjd.testapplicaton;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Handler;
+import android.provider.BaseColumns;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,10 +15,39 @@ public class MainActivity extends AppCompatActivity {
     Intent LeaderBoard;
     Intent Game;
     TextView text;
+
+    /* Inner class that defines the table contents */
+    public static abstract class MarketRow implements BaseColumns {
+        public static final String MARKET = "market";
+        public static final String MARKET_ID = "market_id";
+        public static final String MARKET_NAME = "market_name";
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DataBaseWrapper mDbHelper = new DataBaseWrapper(getBaseContext());
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        String[] marketRecord = {
+                MarketRow.MARKET_ID,
+                MarketRow.MARKET_NAME
+        };
+        Cursor cMarket = db.query(
+                MarketRow.MARKET,       // The table to query
+                marketRecord,           // The columns to return
+                null,                   // The columns for the WHERE clause
+                null,                   // The values for the mDbHelperWHERE clause
+                null,                   // don't group the rows
+                null,                   // don't filter by row groups
+                null                    // The sort order
+        );
+
+        cMarket.moveToFirst();
+        long marketID = cMarket.getLong(cMarket.getColumnIndexOrThrow(MarketRow.MARKET_ID));
+
         LeaderBoard = new Intent(getApplicationContext(), LeaderBoard.class);
         Game = new Intent(getApplicationContext(), GameActivity.class);
 
