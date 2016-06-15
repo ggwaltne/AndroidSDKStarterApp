ec9391bada487e9119f9479464f33cbc64f7c296
Tessa <tessa.seiders@gmail.com>
2016-06-15 15:31:24 -0500

    added clickable sfad.jpg

---
+++ app/src/main/java/com/example/edjd/testapplicaton/VideoAdActivity.java
@@ -0,0 +1,13 @@
+package com.example.edjd.testapplicaton;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class VideoAdActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_ad);
+    }
+}
