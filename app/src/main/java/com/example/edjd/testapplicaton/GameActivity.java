5354b761d87e2d2c37d7eeeeac488ae0869b4a00
Gage Gwaltney <ggwaltne@harding.edu>
2016-06-15 12:34:00 -0700

    Added a die Quickly method to access tessas stuff

--- app/src/main/java/com/example/edjd/testapplicaton/GameActivity.java
+++ app/src/main/java/com/example/edjd/testapplicaton/GameActivity.java
@@ -1,13 +1,21 @@
 package com.example.edjd.testapplicaton;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class GameActivity extends AppCompatActivity {
 
+    Intent LeaderBoard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        LeaderBoard = new Intent(getApplicationContext(), LeaderBoard.class);
+    }
+
+    public void OnClickGameOver (View view) {
+        startActivity(LeaderBoard);
     }
 }
