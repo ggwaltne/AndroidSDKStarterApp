1589fe2f9f48ef6f7c9b98f635b64db9bf7d9ec5
Gage Gwaltney <ggwaltne@harding.edu>
2016-06-15 08:53:46 -0700

    Red Screen

--- app/src/main/java/com/example/edjd/testapplicaton/MainActivity.java
+++ app/src/main/java/com/example/edjd/testapplicaton/MainActivity.java
@@ -13,14 +13,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        text=new TextView(this);
-        text=(TextView)findViewById(R.id.text);
-
     }
-
-    public void OnClick (View view) {
-            text.setText("It's Alive!!!");
-
-    }
-
 }
