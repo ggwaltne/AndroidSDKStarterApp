26e40e45fc78cd7440c3167a019d1ba9c0559e9d
Gage Gwaltney <ggwaltne@harding.edu>
2016-06-15 17:47:58 -0700

    Score data Updated

--- app/src/main/java/com/example/edjd/testapplicaton/GameOverActivity.java
+++ app/src/main/java/com/example/edjd/testapplicaton/GameOverActivity.java
@@ -2,12 +2,19 @@ package com.example.edjd.testapplicaton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GameOverActivity extends AppCompatActivity {
 
+    static public int Score;
+
+    TextView score;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
+        score = (TextView)findViewById(R.id.score);
+        score.setText(Integer.toString(Score));
+
     }
 }
